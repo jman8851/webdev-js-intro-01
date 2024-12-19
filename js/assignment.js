4bf1846a446d0ffbf4132cfea91451b730538203
Jason Caldwell <jman8851@gmail.com>
2024-12-19 14:13:08 -0500

    Placed an X on accomplished task in README
    followed instructions for assignment so favorite book and Activity show when Submit is clicked.
    
    did some experiementing with index and commented out anything that I left to ensure code does not create errors.

--- js/assignment.js
+++ js/assignment.js
@@ -9,28 +9,32 @@ const submissionBtn = document.getElementById("submission-btn")
 
 // Declare a constant variable named "book" and assign
 // the name of your favorite book to that variable.
+const book = "My Little Fuzzy"
 
 
 // Declare a mutable variable named "activity" and assign
 // an activity to it.
+let activity ="Learning New Skills"
 
 
 
 function updateBookSentence() {
     // Sets the text inside the element with the id of book answer. 
     // innerText is one of many properties on a HTML element.
-    bookAnswerEl.innerText = book
+    bookAnswerEl.innerText = book;
 }
 
 function updateActivitySentence() {
     // Update this function 
     // (hint: it will be similiar to the previous function)
+    activityAnswerEl.innerText = activity;
 
 }
 
 function render() {
     // Finish writing this function
     updateBookSentence();
+    updateActivitySentence();
 
 }
 
@@ -38,4 +42,4 @@ submissionBtn.addEventListener("click", function () {
     // Calling the function that renders the update
     // when the button is clicked
     render();
-})
+});
